1cb08af0f5a526d9b3a354f77530b468a3262ee3
umair-ali14 <17-CP-62@students.uettaxila.edu.pk>
2022-04-03 13:45:14 +0500

    cosmetic changes and done/remove btns added

--- main.js
+++ main.js
@@ -9,13 +9,25 @@ submitButton.addEventListener('click', () => {
     else {
         let newTask = document.createElement('div');        // Creating new task element
         newTask.textContent = inputTask.value;              // Assigning user created task to created element
+        newTask.setAttribute('class', 'task');
         tasksContainer.appendChild(newTask);                // Appending new task element to tasks container
-        removeButtonGenerator(newTask);                     // Calling btn generator function
+        doneButton(newTask);                     // Calling btn generator function
+        removeButton(newTask);
     }
 })
 
-function removeButtonGenerator(newTask) {                  // Function to generate remove button with every
+function doneButton(newTask) {                  // Function to generate remove button with every
+    const doneButton = document.createElement('button'); // new task created,
+    doneButton.textContent = "Done";
+    doneButton.setAttribute('class', 'btn-done');                
+    doneButton.addEventListener('click', () => newTask.classList.add('line-through'))
+    newTask.appendChild(doneButton);                     // and then appending the button to new task element
+}
+
+function removeButton(newTask) {                  // Function to generate remove button with every
     const removeButton = document.createElement('button'); // new task created,
-    removeButton.textContent = "Remove Task";              //  
+    removeButton.textContent = "Remove";
+    removeButton.setAttribute('class', 'btn-remove');                
+    removeButton.addEventListener('click', () => newTask.remove())
     newTask.appendChild(removeButton);                     // and then appending the button to new task element
 }
