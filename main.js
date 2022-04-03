21f30804b7f8d26aedd6182cb7cf2dd1109efe9c
umair-ali14 <17-CP-62@students.uettaxila.edu.pk>
2022-04-03 11:36:32 +0500

    added comments to existing js code

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-const submitButton = document.querySelector('#submit');
-const tasksContainer = document.querySelector('.tasks-container');
-const inputTask = document.querySelector('#add-task');
+const submitButton = document.querySelector('#submit');             //
+const tasksContainer = document.querySelector('.tasks-container');  //  Selecting elements from the DOM
+const inputTask = document.querySelector('#add-task');              //
 
 submitButton.addEventListener('click', () => {
-    if (inputTask.value == "") {
+    if (inputTask.value == "") {                                // Adding EventListener to submit task btn
         return 0;
     }
+    else {
+        let newTask = document.createElement('div');        // Creating new task element
+        newTask.textContent = inputTask.value;              // Assigning user created task to created element
+        tasksContainer.appendChild(newTask);                // Appending new task element to tasks container
+        removeButtonGenerator(newTask);                     // Calling btn generator function
+    }
 })
+
+function removeButtonGenerator(newTask) {                  // Function to generate remove button with every
+    const removeButton = document.createElement('button'); // new task created,
+    removeButton.textContent = "Remove Task";              //  
+    newTask.appendChild(removeButton);                     // and then appending the button to new task element
+}
