7423f78b0793431fcf59817af49fe03a18e53f9a
umair-ali14 <17-CP-62@students.uettaxila.edu.pk>
2022-04-04 15:15:46 +0500

    Added more comments

--- main.js
+++ main.js
@@ -9,25 +9,25 @@ submitButton.addEventListener('click', () => {
     else {
         let newTask = document.createElement('div');        // Creating new task element
         newTask.textContent = inputTask.value;              // Assigning user created task to created element
-        newTask.setAttribute('class', 'task');
+        newTask.setAttribute('class', 'task');              // Adding CSS class for styling
         tasksContainer.appendChild(newTask);                // Appending new task element to tasks container
-        doneButton(newTask);                     // Calling btn generator function
-        removeButton(newTask);
+        doneButton(newTask);                                // Calling btn generator function
+        removeButton(newTask);                              //          //
     }
 })
 
-function doneButton(newTask) {                  // Function to generate remove button with every
-    const doneButton = document.createElement('button'); // new task created,
+function doneButton(newTask) {                              // Function to generate done button with every
+    const doneButton = document.createElement('button');    // new task created,
     doneButton.textContent = "Done";
-    doneButton.setAttribute('class', 'btn-done');                
-    doneButton.addEventListener('click', () => newTask.classList.add('line-through'))
+    doneButton.setAttribute('class', 'btn-done');           // Adding CSS for styling     
+    doneButton.addEventListener('click', () => newTask.classList.add('line-through'))   // Adding CSS class
     newTask.appendChild(doneButton);                     // and then appending the button to new task element
 }
 
 function removeButton(newTask) {                  // Function to generate remove button with every
     const removeButton = document.createElement('button'); // new task created,
     removeButton.textContent = "Remove";
-    removeButton.setAttribute('class', 'btn-remove');                
-    removeButton.addEventListener('click', () => newTask.remove())
+    removeButton.setAttribute('class', 'btn-remove');      // Adding CSS class for styling          
+    removeButton.addEventListener('click', () => newTask.remove())  // Removing task on button click
     newTask.appendChild(removeButton);                     // and then appending the button to new task element
 }
