846f267f43470945b4de6f29cb8d0e3b1c277f4a
umair-ali14 <17-CP-62@students.uettaxila.edu.pk>
2022-09-24 20:47:52 +0500

    Successful merge

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
-const client = supabase.createClient('https://vhspscomjwkvdtqgpsqd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoc3BzY29tandrdmR0cWdwc3FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjE0NDg3MjUsImV4cCI6MTk3NzAyNDcyNX0.7SbUAet6KDNJCO4ygiUdHj5XHNBLZ0aV7MrX9bmdDkI')
 const submitButton = document.querySelector('#submit');             //
-const tasksContainer = document.querySelector('.tasks-container');  //  Selecting elements from the DOM
+const tasksContainer = document.querySelector('#container');  //  Selecting elements from the DOM
 const inputTask = document.querySelector('#add-task');              //
 
 
+function add(target) {
+    if (inputTask.value != "") {
+        tasksContainer.innerHTML += `<div id="task" class="p-3 border-b-2 flex justify-between w-96">
+    <p class="w-4/5">${inputTask.value}</p>
+    <div class="flex space-x-2 h-8 w-1/5">
+        <button class="bg-green-500 p-2 rounded-xl hover:bg-green-700"><img class="h-4" src="./edit.svg" alt="edit"></button>
+        <button onclick="remove(this)" class="bg-red-500 p-2 rounded-xl hover:bg-red-700"><img class="h-4" src="./trash.svg" alt="delete"></button>
+    </div>
+</div>`;
+    }
+    else alert("Task field is empty");
+}
 
-// Supabase
+function remove (target) {
+    // console.log(target.parentNode.parentNode);
+    target.parentNode.parentNode.remove();
+}
 
-// console.log(Supabase-js);
 
-// const { createClient } = supabase
-// const _supabase = createClient(
-//     'https://vhspscomjwkvdtqgpsqd.supabase.co', 
-//     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoc3BzY29tandrdmR0cWdwc3FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjE0NDg3MjUsImV4cCI6MTk3NzAyNDcyNX0.7SbUAet6KDNJCO4ygiUdHj5XHNBLZ0aV7MrX9bmdDkI'
-//     )
+function login() {
+    const modal = document.querySelector('#login');
+    modal.showModal();
+}
 
-// Both of the above method of initiating supabase are correct.
+function register() {
+    const modal = document.querySelector('#register');
+    modal.showModal();
+}
 
-//   console.log('Supabase Instance: ', client)
-
-//   console.log(supabase);
-
-// console.log(client);
-// Authorization token: sbp_62b85233c90b9dac211d5f27fd239ce5dc93e7fe
+function closeModal(target) {
+    // const modal = document.querySelector('#login');
+    target.parentNode.close();
+}
